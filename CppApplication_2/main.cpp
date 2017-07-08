7f6a12b2e3591890858246abc1b048d63e9b4183
Yanquan <chenyqkk@outlook.com>
2017-07-08 03:08:32 +0200

    Initial commit

---
+++ CppApplication_2/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: laokuan2017
+ *
+ * Created on July 8, 2017, 3:08 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
